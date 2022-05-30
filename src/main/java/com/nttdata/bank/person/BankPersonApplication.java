44c996e3266e88d2949b79c89e36905b853f2b13
Gian Castro Morales <jesusgianmarco.castromorales@everis.nttdata.com>
2022-05-30 10:26:18 -0500

    commit

--- src/main/java/com/nttdata/bank/person/BankPersonApplication.java
+++ src/main/java/com/nttdata/bank/person/BankPersonApplication.java
@@ -2,8 +2,10 @@ package com.nttdata.bank.person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BankPersonApplication {
 
 	public static void main(String[] args) {
